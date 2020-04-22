87f76ae8e059667910506b65f1e94399941f3310
kalvin <kalvin.vasconcellos@gmail.com>
2020-04-22 11:53:10 -0300

    test: add e2e for payables

---
+++ test/e2e/payable.test.js
@@ -0,0 +1,21 @@
+const test = require('ava');
+const request = require('supertest');
+
+test.beforeEach(async t => {
+
+    const app = require('../../src/server/app');
+
+    t.context.app = app;
+})
+
+test('should return 200 from payables endpoint', async t => {
+
+    const { app } = t.context;
+
+    const res = await request(app)
+        .get('/payables')
+        .send();
+
+    t.is(res.status, 200);
+    t.true(res.body instanceof Array);
+})
