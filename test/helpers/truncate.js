77cc3aba96a3cc9ef301a079d65ed5d1f63245ef
kalvin <kalvin.vasconcellos@gmail.com>
2020-04-21 21:27:41 -0300

    test: add truncate

---
+++ test/helpers/truncate.js
@@ -0,0 +1,8 @@
+
+const sequelize = require('../../src/database');
+
+module.exports = () => {
+    return Promise.all(Object.keys(sequelize.models).map(key => {
+        return sequelize.models[key].destroy({ truncate: true, force: true })
+    }))
+}
