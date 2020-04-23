bdfc7f84c7d00d2d7a3a375c59a942a8aae0b8e1
kalvin <kalvin.vasconcellos@gmail.com>
2020-04-22 23:51:01 -0300

    database: change db name

--- src/database/config.js
+++ src/database/config.js
@@ -5,7 +5,7 @@ module.exports = {
       host: process.env.DB_HOST || 'localhost',
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'psp',
+      database: process.env.DB_NAME || 'postgres',
       define: {
         timestamps: true,
         underscored: true,
@@ -18,7 +18,7 @@ module.exports = {
       host: process.env.DB_HOST || 'localhost',
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'psp',
+      database: process.env.DB_NAME || 'postgres',
       define: {
         timestamps: true,
         underscored: true,
