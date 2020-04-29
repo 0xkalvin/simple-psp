71fc6e8549f18f787843d869eb6eaad2085f0968
kalvin <kalvin.vasconcellos@gmail.com>
2020-04-29 20:56:10 -0300

    database: add indexes for status and transaction id in payable model

---
+++ src/database/migrations/20200429234813-add-payable-status-index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addIndex("payables", ["status"], {
+      concurrently: true,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeIndex("payables", ["status"], {
+      concurrently: true,
+    });
+  },
+};
--- src/resources/payable/model.js
+++ src/resources/payable/model.js
@@ -1,59 +1,63 @@
-const create = (connection, dataTypes) => {
-    return connection.define(
-      "Payable",
-      {
-        id: {
-          type: dataTypes.UUID,
-          defaultValue: dataTypes.UUIDV4,
-          allowNull: false,
-          primaryKey: true,
-        },
-        transactionId: {
-            type: dataTypes.UUID,
-            allowNull: false,
-            field: "transaction_id",
-        },
-        status: {
-            type: dataTypes.ENUM,
-            allowNull: false,
-            values: ['paid', 'waiting_funds'],
-        },
-        paymentDate: {
-            type: dataTypes.DATE,
-            allowNull: false,
-            field: "payment_date",
-        },
-        fee: {
-            type: dataTypes.DOUBLE,
-            allowNull: false,
-        },
-        receivableAmount: {
-            type: dataTypes.DOUBLE,
-            allowNull: false,
-            field: "receivable_amount",
-        },
-        createdAt: {
-          type: dataTypes.DATE,
-          allowNull: false,
-          field: "created_at",
-        },
-        updatedAt: {
-          type: dataTypes.DATE,
-          allowNull: false,
-          field: "updated_at",
-        },
-      },
-      {
-        timestamps: true,
-        freezeTableName: true,
-        tableName: 'payables',
-      }
-    );
-  };
-  
-  const associate = () => {};
-  
-  module.exports = {
-    create,
-  };
-  
+
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  transactionId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: "transaction_id",
+  },
+  status: {
+    type: DataTypes.ENUM,
+    allowNull: false,
+    values: ["paid", "waiting_funds"],
+  },
+  paymentDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: "payment_date",
+  },
+  fee: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  receivableAmount: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+    field: "receivable_amount",
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: "created_at",
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: "updated_at",
+  },
+};
+
+const options = {
+  indexes: [
+    { fields: ["status"] }, 
+    { fields: ["transaction_id"] }
+  ],
+  timestamps: true,
+  freezeTableName: true,
+  tableName: "payables",
+};
+
+
+const create = connection => connection.define("Payable", attributes, options);
+
+
+module.exports = {
+  create,
+};
