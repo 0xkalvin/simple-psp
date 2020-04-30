36b0a267ff055e900a365bdf1fb9d096e2549d9f
Kalvin <kalvin.vasconcellos@gmail.com>
2020-04-29 23:09:55 -0300

    Enhancement/database index (#5)
    Database: add indexes to payable model.
    fields: payment_date, transaction_id, status

---
+++ src/database/migrations/20200429235217-add-payable-transactionId-index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addIndex("payables", ["transaction_id"], {
+      concurrently: true,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeIndex("payables", ["transaction_id"], {
+      concurrently: true,
+    });
+  },
+};
