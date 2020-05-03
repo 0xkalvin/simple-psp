ddf842fdd0fa38dd67d3ed1238c58dbd402060fc
Kalvin <kalvin.vasconcellos@gmail.com>
2020-05-02 22:57:23 -0300

    core: add dashboard feature

---
+++ src/resources/dashboard/index.js
@@ -0,0 +1,17 @@
+const service = require('./service');
+const { buildSuccessResponse } = require('../../lib/http/response');
+
+
+const index = async (req, res, next) => {
+    try {
+        const response = await service.buildDashboard();
+        return buildSuccessResponse(res, 200, response);
+    } catch (err) {
+        return next(err);
+    }
+}
+
+
+module.exports = {
+    index,
+}
