89dc9d6217ac97f3342c40ab07fda59da32ae94e
kalvin <kalvin.vasconcellos@gmail.com>
2020-09-26 23:22:49 -0300

    chore: improve local docker setup

--- src/database/config.js
+++ src/database/config.js
@@ -1,41 +1,20 @@
+const defaultSettings = {
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST,
+  post: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+
+  },
+};
+
 module.exports = {
-  development: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_NAME || 'postgres',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-
-    },
-  },
-  test: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_NAME || 'postgres',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-
-    },
-  },
-  production: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-
-    },
-  },
+  development: defaultSettings,
+  test: defaultSettings,
+  production: defaultSettings,
 };
