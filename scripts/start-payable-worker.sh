8dd4364920f87ebc533ad865aa43e67564a61b56
kalvin <kalvin.vasconcellos@gmail.com>
2020-09-26 23:20:23 -0300

    infra: refactor production dockerfile

--- scripts/start-payable-worker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-node src/bin/payable-worker.js
--- scripts/start-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-node_modules/.bin/sequelize db:migrate --config src/database/config.js --migrations-path src/database/migrations/
-
-node src/bin/server.js
