8e9f37ee32193728e9a24eb0f5e7dce26c270fa9
kalvin <kalvin.vasconcellos@gmail.com>
2020-10-31 09:50:47 -0300

    scripts: improve initialization steps

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $APP_TYPE = "server" ]; then
-    node_modules/.bin/sequelize db:migrate --config src/database/config.js --migrations-path src/database/migrations/
-
-    node src/bin/server.js
-else
-    node src/bin/payable-worker.js
-fi
